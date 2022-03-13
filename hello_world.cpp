9f713f4ec5595248605979b58de0223e82ae8e05
steppans <steppans@mail.ru>
2022-03-13 15:44:48 +0300

    used clang-format to format code

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
-#include <string> // for work with string objects - для работы со строковыми переменными
+#include <string> // for work with string objects
 
-int main(){
-    std::string name;
-    std::cout << "Enter name of user: "; // ask user to enter the name - просит пользователя ввести своё имя
-    std::cin >> name; // input the name into object "name" - запись входных данных в переменную "name"
-    std::cout << "Hello World from " << name << std::endl; // output interactive string - вывод интерактивной строки 
-    return 1;
+int
+main()
+{
+  std::string name;
+  std::cout << "Enter name of user: "; // ask user to enter the name - просит пользователя ввести своё имя
+  std::cin >> name;                    // input the name into object "name" - запись входных данных в переменную "name"
+  std::cout << "Hello World from " << name
+            << std::endl; // output interactive string - вывод интерактивной строки 
+  return 1;
 }
 
