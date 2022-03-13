e566fd88f81ec1ad26844f7acd2bdb0a500a3b2b
steppans <steppans@mail.ru>
2022-03-13 15:01:17 +0300

    added comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <string>
+#include <string> // for work with string objects
 
 int main(){
     std::string name;
-    std::cout << "Enter name of user: ";
-    std::cin >> name;
-    std::cout << "Hello World from " << name << std::endl;
+    std::cout << "Enter name of user: "; // ask user to enter the name
+    std::cin >> name; // input the name into object "name"
+    std::cout << "Hello World from " << name << std::endl; // output interactive string 
     return 1;
 }
 
